5cecc8d5408a20fcefdcc26dbf965e4aa2e1d68c
gcmlabs <giacomo.gb.bonfiglio@gmail.com>
2025-02-04 17:11:37 +0100

    Remove logging from acme.sh and changed DOMAIN var

--- cheap-infra/certManager.sh
+++ cheap-infra/certManager.sh
@@ -4,7 +4,7 @@
 # It uses acme.sh to issue and renew the certificates
 # The script is run by CodeBuild, triggered by a Lambda Custom Resource in the service stack
 # that override the EVENT variable in the startBuild command (Create or Delete)
-# The DOMAIN and ASG_NAME variables are also passed by the lambda to CodeBuild as an environment variable
+# The HOSTNAME and ASG_NAME variables are also passed by the lambda to CodeBuild as an environment variable
 # In all the other cases, the script will renew all the certificates, triggered by a CloudWatch Event
 
 # If the acme.sh is not found in EFS, it will be cloned from the official repository
@@ -14,16 +14,16 @@ if ! [[ -d /traefik/acme ]]; then
     cd /traefik/acme
     git clone https://github.com/acmesh-official/acme.sh.git acme-repo
     cd /traefik/acme/acme-repo
-    ./acme.sh --install --nocron --no-profile --home /traefik/acme --log /traefik/acme/acme.log --syslog 7
+    ./acme.sh --install --nocron --no-profile --home /traefik/acme
 fi
 
 if [[ "$EVENT" == "Create" ]]; then
-    /traefik/acme/acme-repo/acme.sh --issue --server letsencrypt --dns dns_aws -d $DOMAIN --home /traefik/acme --syslog 7
-    cat << EOF > /traefik/etc/${DOMAIN}.yml
+    /traefik/acme/acme-repo/acme.sh --issue --server letsencrypt --dns dns_aws -d $HOSTNAME --home /traefik/acme
+    cat << EOF > /traefik/etc/${HOSTNAME}.yml
 tls:
   certificates:
-    - certFile: /traefik/acme/${DOMAIN}_ecc/fullchain.cer
-      keyFile: /traefik/acme/${DOMAIN}_ecc/${DOMAIN}.key
+    - certFile: /traefik/acme/${HOSTNAME}_ecc/fullchain.cer
+      keyFile: /traefik/acme/${HOSTNAME}_ecc/${HOSTNAME}.key
       stores:
         - default
 EOF
@@ -34,10 +34,10 @@ EOF
         --targets Key=tag:aws:autoscaling:groupName,Values=${ASG_NAME}
 
 elif [[ "$EVENT" == "Delete" ]]; then
-    /traefik/acme/acme-repo/acme.sh --revoke --server letsencrypt -d $DOMAIN --home /traefik/acme --syslog 7
-    /traefik/acme/acme-repo/acme.sh --remove -d $DOMAIN --home /traefik/acme --syslog 7
-    rm -rf /traefik/acme/${DOMAIN}_ecc
-    rm -f /traefik/etc/${DOMAIN}.yml
+    /traefik/acme/acme-repo/acme.sh --revoke --server letsencrypt -d $HOSTNAME --home /traefik/acme
+    /traefik/acme/acme-repo/acme.sh --remove -d $HOSTNAME --home /traefik/acme --syslog 7
+    rm -rf /traefik/acme/${HOSTNAME}_ecc
+    rm -f /traefik/etc/${HOSTNAME}.yml
     sed -i "s/.*Last loaded.*/# Last loaded: $(date)/" /traefik/etc/traefik.yml
     aws ssm send-command \
         --document-name "AWS-RunShellScript" \
@@ -45,7 +45,7 @@ elif [[ "$EVENT" == "Delete" ]]; then
         --targets Key=tag:aws:autoscaling:groupName,Values=${ASG_NAME}
 
 elif [[ "$EVENT" == "Renew" ]]; then
-    /traefik/acme/acme-repo/acme.sh --renew-all --server letsencrypt --home /traefik/acme --syslog 7
+    /traefik/acme/acme-repo/acme.sh --renew-all --server letsencrypt --home /traefik/acme
     sed -i "s/.*Last loaded.*/# Last loaded: $(date)/" /traefik/etc/traefik.yml
     aws ssm send-command \
         --document-name "AWS-RunShellScript" \
